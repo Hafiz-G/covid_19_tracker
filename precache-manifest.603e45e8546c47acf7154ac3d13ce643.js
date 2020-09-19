8eaef49c09f666a263cec75262b6f98e061a849c
Mohammed Hafeez <mdhafizg50@gmail.com>
2020-09-19 06:01:20 +0530

    Updates

---
+++ precache-manifest.603e45e8546c47acf7154ac3d13ce643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5317b37258c94cdf9da4b87128ee7d74",
+    "url": "/covid_19_tracker/index.html"
+  },
+  {
+    "revision": "cd337f9d95716e71febb",
+    "url": "/covid_19_tracker/static/css/main.f9e9f3d6.chunk.css"
+  },
+  {
+    "revision": "9b61381d32eb90b3e0bd",
+    "url": "/covid_19_tracker/static/js/2.b9ee2bb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid_19_tracker/static/js/2.b9ee2bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd337f9d95716e71febb",
+    "url": "/covid_19_tracker/static/js/main.09267121.chunk.js"
+  },
+  {
+    "revision": "71c16c01e62023f4b810",
+    "url": "/covid_19_tracker/static/js/runtime-main.35b39f33.js"
+  }
+]);
